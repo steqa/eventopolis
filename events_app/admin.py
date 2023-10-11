07fb81ce0a97ccce0a070f6d8a521eecfc72334a
Stepan Ivanov <stepan.ivanov.dev@gmail.com>
2023-10-11 20:03:25 +0700

    Add event model

--- events_app/admin.py
+++ events_app/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
 
-from .models import EventCategory
+from .models import Event, EventCategory
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'category', 'latitude', 'longitude')
+    search_fields = ('owner', 'name', 'category')
+    readonly_fields = ('id', 'created_at')
+    fieldsets = (
+        (None,
+         {'fields': (
+             'id',
+             'owner',
+             'members',
+         )}),
+        ('Описание',
+         {'fields': (
+             'name',
+             'description',
+             'category'
+         )}),
+        ('Местоположение',
+         {'fields': (
+             'latitude',
+             'longitude'
+         )}),
+        ('Даты',
+         {'fields': (
+             'created_at',
+         )})
+    )
 
 
 @admin.register(EventCategory)
--- events_app/models.py
+++ events_app/models.py
@@ -1,4 +1,51 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Event(models.Model):
+    owner = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        verbose_name='организатор',
+        on_delete=models.CASCADE
+    )
+    members = models.ManyToManyField(
+        to=settings.AUTH_USER_MODEL,
+        verbose_name='участники',
+        related_name='участники'
+    )
+    name = models.CharField(
+        verbose_name='название',
+        max_length=50
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        max_length=3000
+    )
+    category = models.ForeignKey(
+        to='EventCategory',
+        verbose_name='категория',
+        on_delete=models.CASCADE
+    )
+    latitude = models.FloatField(
+        verbose_name='широта',
+        validators=[MinValueValidator(0.0), MaxValueValidator(90.0)]
+    )
+    longitude = models.FloatField(
+        verbose_name='долгота',
+        validators=[MinValueValidator(0.0), MaxValueValidator(180.0)]
+    )
+    created_at = models.DateTimeField(
+        verbose_name='дата создания',
+        auto_now_add=True
+    )
+    
+    class Meta:
+        verbose_name = 'мероприятие'
+        verbose_name_plural = 'мероприятия'
+    
+    def __str__(self) -> str:
+        return self.name
 
 
 class EventCategory(models.Model):
